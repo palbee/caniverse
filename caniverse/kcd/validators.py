8c398c09af06ce1b318904c6c05b1cbdc8d624a0
Paul Albee <paul@albee.com>
2016-04-24 22:54:13 -0700

    Add validator for Baud Rate

--- caniverse/kcd/validators.py
+++ caniverse/kcd/validators.py
@@ -9,3 +9,12 @@ def validate_bit_length(value):
             _('%(value)s is outside of interval [1,64]'),
             params={'value': value},
         )
+
+
+def check_baud(value):
+    """Ensure that valus is in the interval [5000, 1000000]"""
+    if 5000 <= value <= 1000000:
+        raise ValidationError(
+            _('%(value)s is outside of interval [5000, 1000000]'),
+            params={'value': value},
+        )
