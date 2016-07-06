e6c8c8abcb441c807338b9e0e899cc288356f03e
Paul Albee <paul@albee.com>
2016-07-05 22:49:23 -0700

    Consolidated validators.

--- caniverse/kcd/validators.py
+++ caniverse/kcd/validators.py
@@ -1,20 +1,34 @@
 from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
 from django.utils.translation import ugettext_lazy as _
 
 
-def validate_bit_length(value):
-    """Ensure that value is in the interval [1,64]."""
-    if 1 <= value <= 64:
-        raise ValidationError(
-            _('%(value)s is outside of interval [1,64]'),
-            params={'value': value},
-        )
+@deconstructible
+class RangeValidator(object):
+    """Derived from django.core.validators.BaseValidator"""
+    message = _('Ensure this value is in the interval [%(lower)s, %(upper)s] (it is %(value)s).')
+    code = 'out_of_range'
 
+    def __init__(self, lower, upper, message=None, code=None):
+        self.lower = lower
+        self.upper = upper
 
-def check_baud(value):
-    """Ensure that valus is in the interval [5000, 1000000]"""
-    if 5000 <= value <= 1000000:
-        raise ValidationError(
-            _('%(value)s is outside of interval [5000, 1000000]'),
-            params={'value': value},
+        if message:
+            self.message = message
+
+        if code:
+            self.code = code
+
+    def __call__(self, value):
+        if not self.lower <= value <= self.upper:
+            params = {'lower': self.lower, 'upper': self.upper, 'value': value}
+            raise ValidationError(self.message, code=self.code, params=params)
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__) and
+            (self.lower == other.lower) and
+            (self.upper == other.upper) and
+            (self.message == other.message) and
+            (self.code == other.code)
         )
